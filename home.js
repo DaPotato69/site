3c367cf97653eb6a82c7af31c34e2e6d28249414
DaPotato69 <wenbangalt@gmail.com>
2024-02-08 20:09:48 +1100

    added some stuff, like scroll variable and a content div also the iberra font

--- home.js
+++ home.js
@@ -5,4 +5,9 @@ Array.from(document.getElementsByClassName("menu-item"))
     item.onmouseover = () => {
       menu.dataset.activeIndex = index;
     }
-  });
+});
+
+// --scroll variable
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
