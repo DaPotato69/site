4c88e8b1dad4db5b6549a60ab899ef2dc5bbe11a
DaPotato69 <wenbangalt@gmail.com>
2024-03-16 20:23:46 +1100

    finally made the write an article stuff work

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, request
+import os
+import sqlite3
+from datetime import datetime
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+
+conn = sqlite3.connect('articles.db')
+cur = conn.cursor()
+cur.execute("DROP TABLE IF EXISTS ARTICLES")
+cur.execute("CREATE TABLE ARTICLES (title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, published VARCHAR(255) NOT NULL, article TEXT NOT NULL, imgname VARCHAR(255) NOT NULL)")
+conn.close()
 
 app = Flask(__name__)
+UPLOAD_FOLDER = "./static/blogs/"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'many random bytes'
 
 @app.route('/')
 def home():
@@ -22,8 +36,29 @@ def guide():
 def write():
     return render_template('write.html')
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/write', methods=['POST'])
 def write_post():
-    text = request.form['text']
-    print(text)
-    return render_template('write.html')
+    if 'file' not in request.files:
+        flash('Please upload an image')
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+    conn = sqlite3.connect('articles.db')
+    cur = conn.cursor()
+
+    article = dict(request.form)
+    article["date"] = str(datetime.now().strftime("%d %B %Y"))
+    print(article)
+
+    cur.execute("INSERT INTO ARTICLES VALUES (?, ?, ?, ?, ?)", (article['title'], article['author'], article['date'], article['article'], filename))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('blog'))
