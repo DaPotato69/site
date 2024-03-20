39598f99f402da7ba305ffb467e13674841613d9
DaPotato69 <wenbangalt@gmail.com>
2024-03-20 15:25:46 +1100

    literally got rid of 3 words

--- droptable.py
+++ droptable.py
@@ -2,5 +2,5 @@ import sqlite3
 conn = sqlite3.connect('articles.db')
 cur = conn.cursor()
 cur.execute("DROP TABLE ARTICLES")
-cur.execute("CREATE TABLE IF NOT EXISTS ARTICLES (title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, published VARCHAR(255) NOT NULL, article TEXT NOT NULL, imgname VARCHAR(255) NOT NULL)")
+cur.execute("CREATE TABLE ARTICLES (title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, published VARCHAR(255) NOT NULL, article TEXT NOT NULL, imgname VARCHAR(255) NOT NULL)")
 conn.close()
